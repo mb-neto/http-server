9680896e8f24f7367917c65f55ae0587aae55f75
Manoel Benedito Neto <manoelneto164@gmail.com>
2020-10-29 22:55:32 -0300

    corrige problema de fstring

--- HTTPserver.py
+++ HTTPserver.py
@@ -16,22 +16,22 @@ print ('Serving HTTP on port %s ...' % PORT)
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    array = request.decode('utf-8').split()
+    array = request.decode('utf-8').strip().split()
     http_response = str()
     sentence = str()
+
     if array[1]:
-        url = array[1].split('/')
-        if url[0] == 'index.html':
-            http_response = """HTTP/1.1 200 OK \r \n \r \n"""
-            page = open('pages/index.html', 'r')
+        url = array[1]
+        if url == '/index.html':
+            http_response = """HTTP/1.1 200 OK\r\n\r\n"""
+            page = open(f'pages/index.html', 'r')
             sentence = page.read()
             page.close()
         else:
-            http_response = """HTTP/1.1 404 Not found \r \n \r \n"""
-            page = open('pages/not_found.html', 'r')
+            http_response = """HTTP/1.1 404 Not found\r\n\r\n"""
+            page = open(f'pages/not_found.html', 'r')
             sentence = page.read()
             page.close()
-        print(http_response + sentence)
-        client_connection.send((http_response + sentence).encode('utf-8'))
+    client_connection.send((http_response + sentence).encode('utf-8'))
     client_connection.close()
 listen_socket.close()
